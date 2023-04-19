21cf13ddf4af98f82d4fb2b0d54803ef3d03ab8c
Chandan <chandanms1312@gmail.com>
2023-04-19 15:26:54 +0530

    commit version 2

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
+        baseUrl: "https://mern-app-backend-wmdm.onrender.com",
     }),
 
     endpoints: (builder) => ({
